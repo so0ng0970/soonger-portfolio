85dfdd1ae1a38a3407eb86517d92f79a413bfab6
soonger3306 <soonger3306@daum.net>
2024-01-02 23:57:19 +0900

    feat: ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
 import { RecoilRoot } from "recoil";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,27 +1,56 @@
 import styled, { keyframes } from "styled-components";
-import { Card } from "antd";
+import { Card, Row, Col } from "antd";
 
 const { Meta } = Card;
 function Project() {
   return (
     <Container>
-      <Card
-        hoverable
-        style={{
-          width: 240,
-        }}
-        cover={
-          <img
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Meta title="Europe Street beat" description="www.instagram.com" />
-      </Card>
+      <ProjectContainer>
+        <CardComponent
+          backgroundImage="assets/images/project1.jpg"
+          text="You & I Diary"
+        />
+        <CardComponent
+          backgroundImage="assets/images/background1.jpg"
+          text="MAAP"
+        />
+        <CardComponent
+          backgroundImage="assets/images/background1.jpg"
+          text="STOCK'S TALK"
+        />
+        <CardComponent
+          backgroundImage="assets/images/background1.jpg"
+          text="PORTFOLIO"
+        />
+      </ProjectContainer>
     </Container>
   );
 }
+export const CardComponent = ({
+  key,
+  title,
+  description,
+  backgroundImage,
+  text,
+}) => {
+  return (
+    <CardWrapper>
+      <TitleComponent text={text} />
+      <Card
+        key={key}
+        hoverable
+        style={{ backgroundColor: "transparent", height: 300, width: 250 }}
+        cover={<ProjectImage backgroundImage={backgroundImage} />}
+      />
+    </CardWrapper>
+  );
+};
+export const TitleComponent = ({ text }) => (
+  <DivContainer>
+    <ColDiv />
+    <Title>{text}</Title>
+  </DivContainer>
+);
 export default Project;
 
 export const Container = styled.div`
@@ -40,5 +69,74 @@ export const Container = styled.div`
   }
 
   @media (max-width: 790px) {
+  }
+`;
+
+const ProjectContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+
+  @media (max-width: 790px) {
+    flex-direction: row;
+  }
+`;
+
+export const ProjectImage = styled.div`
+  background-image: url(${(props) => props.backgroundImage});
+  background-size: cover;
+  background-position: center;
+  width: 300px;
+  height: 300px;
+  @media (max-width: 1500px) {
+    width: 200px;
+    height: 280px;
+  }
+  @media (max-width: 977px) {
+    width: 200px;
+    height: 280px;
+  }
+  @media (max-width: 790px) {
+    width: 200px;
+    height: 280px;
+  }
+`;
+const CardWrapper = styled.div`
+  &:nth-of-type(even) {
+    position: relative;
+    top: 2rem;
+  }
+`;
+
+export const DivContainer = styled.div`
+  width: 250px;
+  height: 100px;
+  @media (max-width: 977px) {
+    width: 250px;
+  }
+  @media (max-width: 790px) {
+    font-size: 13px;
+    width: 300px;
+    height: 20px;
+  }
+`;
+export const ColDiv = styled.div`
+  background-color: #003b57;
+  width: 250px;
+  height: 20px;
+`;
+
+export const Title = styled.p`
+  color: #000000;
+  font-family: "Scalter-SerifCondSlant";
+  font-size: 25px;
+  @media (max-width: 977px) {
+    font-size: 16px;
+  }
+  @media (max-width: 790px) {
+    font-size: 14px;
   }
 `;
